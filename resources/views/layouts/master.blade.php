d2fd55e987de465ef38744d62dce5ddd8078577a
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-21 11:40:24 +0300

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -156,6 +156,12 @@
             </div>
 
             @if(Entrust::hasRole('administrator') || Entrust::hasRole('owner'))
+                <a href="{{ route() }}" class=" list-group-item" data-toggle="collapse" data-parent="#MainMenu"><i
+                            class="fa fa-cog sidebar-icon"></i><span id="menu-txt">{{ __('Reinitialiser') }}</span>
+                    
+            @endif
+
+            @if(Entrust::hasRole('administrator') || Entrust::hasRole('owner'))
                 <a href="#settings" class=" list-group-item" data-toggle="collapse" data-parent="#MainMenu"><i
                             class="fa fa-cog sidebar-icon"></i><span id="menu-txt">{{ __('Settings') }}</span>
                     <i class="icon ion-md-arrow-dropup arrow-side sidebar-arrow"></i></a>
