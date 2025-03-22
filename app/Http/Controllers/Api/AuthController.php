afb7263dbf1de22eecf031945845e0ae7dbcbc9a
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-22 14:23:36 +0300

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,34 +12,6 @@ use Laravel\Sanctum\HasApiTokens;
 
 class AuthController extends Controller
 {
-    // Inscription
-    public function register(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
-        }
-
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        $token = $user->createToken('API Token')->plainTextToken;
-
-        return response()->json([
-            'user' => $user,
-            'token' => $token,
-        ]);
-    }
-
-    // Connexion
     public function login(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -57,11 +29,10 @@ class AuthController extends Controller
             return response()->json(['message' => 'Les identifiants sont incorrects'], 401);
         }
 
-        $token = $user->createToken('API Token')->plainTextToken;
+        // $token = $user->createToken('API Token')->plainTextToken;
 
         return response()->json([
-            'user' => $user,
-            'token' => $token,
+            'user' => $user
         ]);
     }
 
