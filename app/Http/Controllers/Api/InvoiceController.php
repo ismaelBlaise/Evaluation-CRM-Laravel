37e5b5079810050aa58605b5d0367cae068c1f40
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-23 10:02:38 +0300

    graph 3

--- app/Http/Controllers/Api/InvoiceController.php
+++ app/Http/Controllers/Api/InvoiceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Invoice;
+use App\Services\Invoice\InvoiceCalculator;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
@@ -22,4 +23,33 @@ class InvoiceController extends Controller
             "nb_invoices" => Invoice::count()
         ]);
     }
+
+    public function invoicePaymentSummary()
+    {
+        $totalPaid = 0;
+        $totalUnpaid = 0;
+
+        
+        
+        $invoices = Invoice::all();
+
+        foreach ($invoices as $invoice) {
+            $invoiceCalculator = new InvoiceCalculator($invoice);
+            $amountDue = $invoiceCalculator->getAmountDue(); 
+            $totalPayments = $invoice->payments()->sum('amount');  
+
+            if ($amountDue->getBigDecimalAmount() == 0) {
+                $totalPaid += $totalPayments;  
+            } else {
+                $totalUnpaid += $amountDue->getBigDecimalAmount(); 
+            }
+        }
+
+        return response()->json([
+            'total_paid' => $totalPaid,
+            'total_unpaid' => $totalUnpaid,
+        ]);
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -60,6 +60,7 @@ Route::prefix('offers')->group(function () {
 Route::prefix('invoices')->group(function () {
     Route::get('/', [InvoiceController::class, 'data']);
     Route::get('/nb', [InvoiceController::class, 'nbdata']);
+    Route::get('/chart', [InvoiceController::class, 'invoicePaymentSummary']);
 });
 
 // Payment Routes
