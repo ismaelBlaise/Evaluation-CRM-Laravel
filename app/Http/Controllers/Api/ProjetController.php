1483010dc924beee9e12148ad9a0a457a3d51c8f
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-23 07:23:50 +0300

    Update ProjetController.php

--- app/Http/Controllers/Api/ProjetController.php
+++ app/Http/Controllers/Api/ProjetController.php
@@ -28,18 +28,25 @@ class ProjetController extends Controller
 
     public function getProjectCountByStatus()
     {
-         
+        $allStatuses = Status::where('source_type', 'App\Models\Project')->get();
+        
         $projectCounts = Project::select('status_id', \DB::raw('count(*) as total'))
             ->groupBy('status_id')
             ->get()
             ->mapWithKeys(function ($item) {
-                 
                 $status = Status::find($item->status_id);
                 return [$status ? $status->title : 'Unknown' => $item->total];
             });
 
-         
+        foreach ($allStatuses as $status) {
+            if (!isset($projectCounts[$status->title])) {
+                $projectCounts[$status->title] = 0;
+            }
+        }
+
         return response()->json($projectCounts);
     }
+
+
 
 }
